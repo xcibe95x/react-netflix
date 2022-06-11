2eb83721832babe836c7e6b0e8e1c40be2af2878
xcibe95x <xcibe95x@gmail.com>
2022-06-12 01:37:10 +0200

    Clean up and Setup React Project

---
+++ src/Godflex.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Godflex.css';
+
+// Godflex Home Page
+
+function Godflex() {
+  return (
+    <div className="Godflex">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default Godflex;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './Godflex.css';
+import Godflex from './Godflex';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Godflex />
   </React.StrictMode>
 );
 
